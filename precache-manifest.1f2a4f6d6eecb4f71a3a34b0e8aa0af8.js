8e17e50fe67bcc0a477b715a561d303073ee27cd
pablotruji <andrestrujillocruz.1989@gmail.com>
2021-07-26 12:01:19 -0500

    Updates

---
+++ precache-manifest.1f2a4f6d6eecb4f71a3a34b0e8aa0af8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "f246f5f1fb702016606ee8c65e33c029",
+    "url": "/platzi-badges-v-truji/static/media/platziconf-logo.f246f5f1.svg"
+  },
+  {
+    "revision": "01d80ca39e83a2a8d3e1",
+    "url": "/platzi-badges-v-truji/static/css/main.23dc427d.chunk.css"
+  },
+  {
+    "revision": "308ed1f047a6c24e4c70",
+    "url": "/platzi-badges-v-truji/static/js/runtime~main.308ed1f0.js"
+  },
+  {
+    "revision": "fa24d6804713bab3148bb521e46339a7",
+    "url": "/platzi-badges-v-truji/static/media/Twitter_social_icons_circle_blue.fa24d680.svg"
+  },
+  {
+    "revision": "93284489bc930de62e94",
+    "url": "/platzi-badges-v-truji/static/js/2.93284489.chunk.js"
+  },
+  {
+    "revision": "01d80ca39e83a2a8d3e1",
+    "url": "/platzi-badges-v-truji/static/js/main.01d80ca3.chunk.js"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/platzi-badges-v-truji/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/platzi-badges-v-truji/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/platzi-badges-v-truji/static/media/stars.1d17360a.svg"
+  },
+  {
+    "revision": "93284489bc930de62e94",
+    "url": "/platzi-badges-v-truji/static/css/2.52eab499.chunk.css"
+  },
+  {
+    "revision": "fc0c552c97c962fe37b35fc79b93f72d",
+    "url": "/platzi-badges-v-truji/index.html"
+  }
+];
